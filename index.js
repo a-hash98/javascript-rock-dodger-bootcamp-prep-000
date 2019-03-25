32326d50874bd8043dd199cb8f17826174c861cc
Ay Has <ahashim983@yahoo.com>
2019-03-25 02:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -130,8 +130,7 @@ function createRock(x = 2) {
 function endGame() {
   clearInterval(gameInterval)
   for (var i = 0; i < ROCKS.length; i++){
-    ROCKS[i].parentNode.removeChild(Rocks[i])
-  }
+    ROCKS[i] = ROCKS[i]:after
   alert('YOU LOSE!')
 }
 
